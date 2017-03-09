7fef3f62cb66452e6fd6d4a35912bb039b3f43a5
Milos Vasic <milos85vasic@gmail.com>
2017-03-09 10:26:57 +0100

    Groot Tryouts: More Groot Android stuff.

--- Groot Android Tryout/Tryout_Android_Library/src/main/java/net/milosvasic/tryout/groot/android/GrootTryoutActivity.java
+++ Groot Android Tryout/Tryout_Android_Library/src/main/java/net/milosvasic/tryout/groot/android/GrootTryoutActivity.java
@@ -5,12 +5,17 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 
+import com.google.gson.Gson;
+
 public class GrootTryoutActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
         setContentView(R.layout.activity_groot_tryout);
+
+        // Let's try JARs:
+        Gson gson = new Gson();
     }
 
 }
