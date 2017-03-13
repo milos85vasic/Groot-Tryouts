eb2ee4314c4434712163a41061b8ee76fcdfc9f0
Milos Vasic <milos85vasic@gmail.com>
2017-03-13 07:34:59 +0100

    Groot Tryouts: Android, library - no flavors.

---
+++ Groot Android Libraries Tryout/Tryout_Android_Library_No_Flavors/src/main/java/net/milosvasic/tryout/groot/android/library/no_flavors/NoFlavorsTryoutClass.java
@@ -0,0 +1,16 @@
+package net.milosvasic.tryout.groot.android.library.no_flavors;
+
+
+abstract class NoFlavorsTryoutClass {
+
+    private String value;
+
+    public NoFlavorsTryoutClass(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+}
