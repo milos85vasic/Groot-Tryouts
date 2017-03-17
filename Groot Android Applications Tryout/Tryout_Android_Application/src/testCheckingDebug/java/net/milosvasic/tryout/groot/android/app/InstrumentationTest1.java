20811746044c103e158d7858956c00903466d549
Milos Vasic <milos85vasic@gmail.com>
2017-03-17 20:17:36 +0100

    Groot Tryouts: Extending examples.

---
+++ Groot Android Applications Tryout/Tryout_Android_Application/src/testCheckingDebug/java/net/milosvasic/tryout/groot/android/app/InstrumentationTest1.java
@@ -0,0 +1,14 @@
+package net.milosvasic.tryout.groot.android.app;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class InstrumentationTest1 {
+
+    @Test
+    public void test() {
+        Assert.assertEquals(2, 2);
+    }
+
+}
