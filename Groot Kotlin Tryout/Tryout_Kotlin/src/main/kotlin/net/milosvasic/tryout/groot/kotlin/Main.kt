69b50ef0b69a999f37b703fd63710343be54f8a9
Milos Vasic <milos85vasic@gmail.com>
2017-03-19 11:24:59 +0100

    Groot Tryouts: Updated examples.

--- Groot Kotlin Tryout/Tryout_Kotlin/src/main/kotlin/net/milosvasic/tryout/groot/kotlin/Main.kt
+++ Groot Kotlin Tryout/Tryout_Kotlin/src/main/kotlin/net/milosvasic/tryout/groot/kotlin/Main.kt
@@ -1,23 +1,15 @@
 package net.milosvasic.tryout.groot.kotlin
 
-import net.milosvasic.factory.project.ProjectFactory
-import net.milosvasic.logger.ConsoleLogger
-
-private class Factory : ProjectFactory()
-
 fun main(args: Array<String>) {
 
     println("Trying out Groot.")
-    println("${BuildConfig.NAME} ${BuildConfig.VERSION}")
-
-    val TAG = Factory::class
-    val logger = ConsoleLogger()
+    println("${BuildConfig.NAME} ${BuildConfig.VERSION} ${BuildConfig.VARIANT}")
 
     val items = mutableListOf("Lion", "Elephant")
     items.add("Cow")
-    items.forEachIndexed { i, s -> logger.c(TAG, s) }
+    items.forEach(::println)
 
     val car = Car("Fiat", "Benzin")
-    logger.n(TAG, car.toString())
+    println(car.toString())
 
 }
