6b4a51f5312c5fc80def35cbdb1ddc5db70529aa
Milos Vasic <milos85vasic@gmail.com>
2017-03-20 12:47:30 +0100

    Groot Tryouts: Covering all flavors so build is working properly.

--- Groot Android Libraries Tryout/Tryout_Android_Library/src/main/java/net/milosvasic/tryout/groot/android/library/TestActivity1.java
+++ Groot Android Libraries Tryout/Tryout_Android_Library/src/main/java/net/milosvasic/tryout/groot/android/library/TestActivity1.java
@@ -5,10 +5,10 @@ import android.app.Activity;
 
 public class TestActivity1 extends Activity {
 
-//    private ExampleClass1 exampleClass1;
-//
-//    {
-//        exampleClass1 = new ExampleClass1();
-//    }
+    private ExampleClass1 exampleClass1;
+
+    {
+        exampleClass1 = new ExampleClass1();
+    }
 
 }
